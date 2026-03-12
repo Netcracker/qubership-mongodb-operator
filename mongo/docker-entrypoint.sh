bd6d54ca00c9092ab5b519c1ea5cb63d89f26130
Shubham Bhadoria <shubham.bhadoria@netcracker.com>
2026-03-12 17:23:09 +0530

    fix

--- mongo/docker-entrypoint.sh
+++ mongo/docker-entrypoint.sh
@@ -4,5 +4,7 @@ set -e
 # Fix ownership every time on startup (optional, helpful if using mounted volumes)
 chown -R mongodb:mongodb /data/db /data/configdb
 
+echo "test"
+
 # Run mongod as user 'mongodb' (UID 1001)
 exec gosu mongodb "$@"
