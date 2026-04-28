ba7424e2ab1130143904f4fa2e47b9edc5f5fea3
Shubham Bhadoria <shubham.bhadoria@netcracker.com>
2026-04-28 23:07:22 +0530

    fix: added oplog size

--- operator/api/v1alpha1/mongodbdeployment_types.go
+++ operator/api/v1alpha1/mongodbdeployment_types.go
@@ -136,6 +136,8 @@ type MongoDB struct {
 	Storage                 *types.StorageRequirements `json:"storage,omitempty"`
 	CnfWiredTigerCacheGb    string                     `json:"cnfWiredTigerCacheGb,omitempty"`
 	DataWiredTigerCacheGb   string                     `json:"dataWiredTigerCacheGb,omitempty"`
+	CnfOpLogSizeMb          int                        `json:"cnfOpLogSizeMb,omitempty"`
+	DataOpLogSizeMb         int                        `json:"dataOpLogSizeMb,omitempty"`
 	SingleWiredTigerCacheGb string                     `json:"singleWiredTigerCacheGb,omitempty"`
 	AdditionalNodeLabels    map[string]string          `json:"additionalNodeLabels,omitempty"`
 	CustomDataRSParameters  []string                   `json:"customDataRSParameters,omitempty"`
