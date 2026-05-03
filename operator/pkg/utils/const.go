73d3a450c176f847678e4b47bcd36c7fada27dcf
Shubham Bhadoria <shubham.bhadoria@netcracker.com>
2026-05-03 12:41:38 +0530

    fix: command update

--- operator/pkg/utils/const.go
+++ operator/pkg/utils/const.go
@@ -178,7 +178,7 @@ const JsReplicasWithStateAndName = "rs.status().members.map(function(m) { return
 
 const shardHostsTemplate = "JSON.stringify(db.getSiblingDB('config').shards.updateOne( {'_id' : '%s'}, {\\$set:{ host: '%s/%s', state: 1}}))"
 
-const JsOpLogSize = "db.getSiblingDB(\"%s\").oplog.rs.stats().maxSize"
+const JsOpLogSize = "db.getSiblingDB('%s').oplog.rs.stats().maxSize"
 
 const shardTemplate = "" +
 	"adminDB=db.getSiblingDB('local');" +
