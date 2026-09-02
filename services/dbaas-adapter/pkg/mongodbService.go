c84e974d57692a1e827071c9288c46d789cd1784
Shubham Bhadoria <shubham.bhadoria@netcracker.com>
2026-09-02 20:38:46 +0530

    fix: updated

--- services/dbaas-adapter/pkg/mongodbService.go
+++ services/dbaas-adapter/pkg/mongodbService.go
@@ -622,7 +622,7 @@ func (r *MongoConfigurationImpl) IsTLSEnabled() bool {
 }
 
 func (r *MongoServiceImpl) EnableShardingAndCreateCollection(ctx context.Context, dbName string, settings *mUtils.Settings) error {
-	err := r.RunWithGrants(ctx, "admin", func(service MongoService) error {
+	err := r.RunWithClusterGrants(ctx, "admin", func(service MongoService) error {
 		err := service.EnableSharding(ctx, dbName)
 		if err != nil && !strings.Contains(err.Error(), "already enabled") {
 			return fmt.Errorf("failed to enable sharding on db %s: %w", dbName, err)
@@ -800,7 +800,7 @@ func (r *MongoServiceImpl) ShardNonShardedCollection(ctx context.Context, dbName
 		}
 	}
 
-	err = r.RunWithClusterGrants(ctx, dbName, func(service MongoService) error {
+	err = r.RunWithGrants(ctx, dbName, func(service MongoService) error {
 		return service.CreateShardKeyIndex(ctx, dbName, settings)
 	})
 	if err != nil {
